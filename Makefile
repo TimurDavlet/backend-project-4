5faad444f11ce1f9832409bb9cf94cac9455977f
Timur D <timkoootim@yandex.ru>
2026-04-13 14:14:40 +0300

    подготовка

---
+++ Makefile
@@ -0,0 +1,11 @@
+install:
+	npm ci
+
+page-loader:
+	node bin/page-loader.js
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
---
+++ bin/page-loader.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Page Loader')
+const url = readlineSync.question('Enter the page URL: ');
